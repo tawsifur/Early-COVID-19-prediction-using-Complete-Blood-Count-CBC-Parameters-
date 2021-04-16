eb34bb6f6f8e26627547b51ab1ad7bfa9d6ca7a9
Tawsifur Rahman <56173953+tawsifur@users.noreply.github.com>
2021-04-16 19:01:42 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def predict():
         
         output= -12.75911+0.5784669*Neutrophils_Count+0.0724752*Age-0.009611*Platelet_Count+0.0931182*Monocytes+0.0064276*White_Blood_Cell_Count-3.567051*Lymphocyte_Count+0.7140086*Red_Blood_Cell_Distribution_Width 
         dp=1/(1+math.exp(-output))
-        
+        dp=round(dp,4)
         return render_template('result.html', prediction=dp)
 
 if __name__ == '__main__':
